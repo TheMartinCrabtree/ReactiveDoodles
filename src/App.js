35306a53738638600e5ef5831c81c96035d8a2d2
Martin Crabtree <mcrabtr33@gmail.com>
2020-02-11 17:34:06 -0500

    testing login button

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Homepage from './Homepage';
+import Login from './Login';
+
 function App() {
   // Basic login for now
   const [loginVerified, setLoginVerified] = useState(false);
+
+  const verifyLogin=()=>{
+    if(loginVerified){
+      return(
+        <Homepage />
+      );
+    }else{
+      return(
+        <Login  setLoginVerified={setLoginVerified} />
+      );
+    }
+
+  };
 
 
   return (
     <div >
       <header >
         <h3>Derpy Doodles</h3>
+        { verifyLogin }
         
       </header>
     </div>
--- src/Login.js
+++ src/Login.js
@@ -6,6 +6,7 @@ const Login=(props)=>{
     return(
         <section>
             <h4>Please login:</h4>
+            <button id="login" onClick={props.setLoginVerfied(true)} >Login</button>
         </section>
     );
 }
