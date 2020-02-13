efd79387749d82d682f18447a98458b143e6887d
MC-Games\MC <martin.crabtree@gmail.com>
2020-02-12 22:51:19 -0500

    some demo code made in Temp

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -1,15 +1,16 @@
+
 import React, { useContext, useEffect } from 'react';
 import { Context } from '../Store';
 
 const SetUsername=(props)=>{
-    const [store, dispatch] = useContext(Context);
+    const [state, dispatch] = useContext(Context);
     const currentUsername = "BOBBY";
 
-    // useEffect(()=>{
-    //     if(currentUsername){
-    //         dispatch({ type: 'UPDATE_NAME', currentUsername })
-    //     }
-    // })
+    useEffect(()=>{
+        if(currentUsername){
+            dispatch({ type: 'UPDATE_NAME', currentUsername })
+        }
+    })
 
     return(
         <>  </>
@@ -17,3 +18,6 @@ const SetUsername=(props)=>{
 }
 
 export default SetUsername;
+
+
+
